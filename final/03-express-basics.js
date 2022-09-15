94ad9bec0d274f920c16170367e7274163e5d369
Chormun <chormunlam@yahoo.com.hk>
2022-09-15 01:01:09 -0400

    v5

--- final/03-express-basics.js
+++ final/03-express-basics.js
@@ -1,27 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express')//import the module, express libray, 
+const app =express()//invoke it
+//or you can do in 1 line
+//const app =rquire('express')()
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+
+app.listen(5000,()=>{//later will show how to do it dynamic
+  console.log("server is listening on part 5000");
 })
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+
+app.get('/',(req, res)=>{
+  console.log('user hit the resouce')
+  res.status(200).send('Home page')
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+app.get('/about', (req, res)=>{
+  res.status(200).send('about page')
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+app.all('*', (req,res)=>{
+
+  res.status(404).send('<h1>resouce not found</h1>')
 })
 
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
+
+
+//app.get  read data
+//app.post   insert data
+//app.put    update data
+//app.delete  delete data
+//app.all     work on all of them 
+//app.use
+//app.listen
+
+//why i got 304 instead of 200
--- final/05-all-static.js
+++ final/05-all-static.js
@@ -4,13 +4,14 @@ const path = require('path')
 const app = express()
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static('./public'))//all work...dont need set up 
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 //   adding to static assets
 //   SSR
 // })
+//still work
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
